47d24e6a43910550b2fff01951e7ea2216932166
Fadhel-Abbas <ed.solutions10@gmail.com>
2025-02-15 17:48:58 -0800

    Add files via upload

---
+++ blink.asm
@@ -0,0 +1,34 @@
+;blink.s
+;turns on an LED which is connected to PB5 (digital out 13)
+
+.include "./m328Pdef.inc"
+rjmp Start
+
+Start:
+	call On
+
+	; set delay to max possible in 16 bit number
+	; and call subroutine Delay
+	ldi zl, 0xFF
+	ldi zh, 0xFF 
+	call Delay
+
+	call Off
+	rjmp Start
+
+Delay:
+	sbci zl, 0x01
+	brne Delay; load delay size in Z-register
+	ret
+
+On:
+	ldi r16, 0b00100000
+	out DDRB, r16
+	out PortB, r16
+	ret
+
+Off:
+	ldi r16, 0b00000000
+	out DDRB, r16
+	out PortB, r16
+	ret
